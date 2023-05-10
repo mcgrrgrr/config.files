6b64c4b165d1786f4900b11c5c5ac889030cea10
ian <105318201+mcgrrgrr@users.noreply.github.com>
2023-05-09 20:40:30 -0500

    Add function to open selected file with xdg-open for image previewing

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>o', function()
+    local selection = require('telescope.actions.state').get_selected_entry()
+    vim.fn.jobstart("xdg-open " .. selection.path)
 end)
